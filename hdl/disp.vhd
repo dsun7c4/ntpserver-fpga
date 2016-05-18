610862366ba6f8942bf651f848312f8ba8879d64
Daniel Sun <dcsun88osh@gmail.com>
2016-05-17 23:32:06 -0400

    Block memory frame buffer and lookup table.

--- hdl/disp.vhd
+++ hdl/disp.vhd
@@ -6,7 +6,7 @@
 -- Author     : Daniel Sun  <dcsun88osh@gmail.com>
 -- Company    : 
 -- Created    : 2016-05-14
--- Last update: 2016-05-15
+-- Last update: 2016-05-17
 -- Platform   : 
 -- Standard   : VHDL'93
 -------------------------------------------------------------------------------
@@ -38,10 +38,15 @@ entity disp is
       tsc_1ppus         : in    std_logic;
 
       disp_pdm          : in    std_logic_vector(7 downto 0);
-      dp                : in    std_logic_vector(31 downto 0);
-      disp_data         : in    std_logic_vector(31 downto 0);
-      disp_addr         : out   std_logic_vector(4 downto 0);
 
+      -- Display memory
+      dp                : in    std_logic_vector(31 downto 0);
+      cpu_addr          : in    std_logic_vector(9 downto 0);
+      cpu_we            : in    std_logic;
+      cpu_datao         : in    std_logic_vector(31 downto 0);
+      cpu_data1         : out   std_logic_vector(31 downto 0);
+
+      -- Time of day
       t_1ms             : in    std_logic_vector(3 downto 0);
       t_10ms            : in    std_logic_vector(3 downto 0);
       t_100ms           : in    std_logic_vector(3 downto 0);
@@ -55,6 +60,7 @@ entity disp is
       t_1h              : in    std_logic_vector(3 downto 0);
       t_10h             : in    std_logic_vector(3 downto 0);
 
+      -- Output to tlc59282 LED driver
       disp_sclk         : OUT   std_logic;
       disp_blank        : OUT   std_logic;
       disp_lat          : OUT   std_logic;
@@ -66,6 +72,69 @@ end disp;
 
 
 architecture rtl of disp is
+
+    component disp_sr
+        port (
+            rst_n             : in    std_logic;
+            clk               : in    std_logic;
+
+            tsc_1pps          : in    std_logic;
+            tsc_1ppms         : in    std_logic;
+            tsc_1ppus         : in    std_logic;
+
+            disp_data         : in    std_logic_vector(255 downto 0);
+
+            disp_sclk         : OUT   std_logic;
+            disp_lat          : OUT   std_logic;
+            disp_sin          : OUT   std_logic
+
+            );
+    end component;
+
+
+    component disp_lut
+        port (
+            rst_n             : in    std_logic;
+            clk               : in    std_logic;
+
+            cpu_addr          : in    std_logic_vector(9 downto 0);
+            cpu_we            : in    std_logic;
+            cpu_datao         : in    std_logic_vector(31 downto 0);
+            cpu_data1         : out   std_logic_vector(31 downto 0);
+
+            disp_addr         : in    std_logic_vector(11 downto 0);
+            disp_data         : in    std_logic_vector(7 downto 0)
+            );
+    end component;
+
+
+    component disp_pdm
+        port (
+            rst_n             : in    std_logic;
+            clk               : in    std_logic;
+
+            tsc_1ppms         : in    std_logic;
+
+            disp_pdm          : in    std_logic_vector(7 downto 0);
+
+            disp_blank        : OUT   std_logic
+            );
+    end component;
+
+
+    component disp_state
+        port (
+            rst_n             : in    std_logic;
+            clk               : in    std_logic;
+
+            tsc_1ppms         : in    std_logic;
+
+
+            );
+    end component;
+
+
+    SIGNAL disp_data    : std_logic_vector(255 downto 0);
 
 
 begin
